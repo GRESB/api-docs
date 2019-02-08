93292540846e7f85a45fec33dd5324e65a8b2536
Theodoros Orfanidis <t.orfanidis@gresb.com>
2019-02-08 15:49:53 +0100

    Separate links from field descriptions

--- lib/markdown_erb.rb
+++ lib/markdown_erb.rb
@@ -6,4 +6,14 @@ module MarkdownErb
   def md_renderer
     Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
+
+  def dictionary_description(field)
+    desc = data.dictionary.descriptions[field]
+    link = data.dictionary.links[field]
+    if link
+      markdown("#{desc} See #{link} for further information.")
+    else
+      markdown(desc)
+    end
+  end
 end
